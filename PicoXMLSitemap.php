d38b17570bbbe31c262ec34335f5bcb254d49489
Robby <Robby-@users.noreply.github.com>
2016-05-05 06:56:06 +0200

    Set the correct Content-Type HTTP header, and do it only once.

--- PicoXMLSitemap.php
+++ PicoXMLSitemap.php
@@ -65,8 +65,8 @@ class PicoXMLSitemap extends AbstractPicoPlugin
         if($this->is_sitemap){
             //Sitemap found, 200 OK
             header($_SERVER['SERVER_PROTOCOL'].' 200 OK');
-            //Set content-type to application/xml
-            header('Content-Type: application/xml; charset=UTF-8');
+            //Set content-type to text/xml
+            header('Content-Type: text/xml; charset=UTF-8');
             //XML Start
             $xml = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
             //Page loop
@@ -81,8 +81,6 @@ class PicoXMLSitemap extends AbstractPicoPlugin
             }
             //XML End
             $xml .= '</urlset>';
-            //Set content-type to text/xml
-            header('Content-Type: text/xml');
             //Show generated sitemap
             die($xml);
         }
